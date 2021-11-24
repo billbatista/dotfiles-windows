ac1585033cc5defd432d2c5cd0bc705b8e0bb6dd
William M. Batista <william.batista@ambevtech.com.br>
2021-11-24 14:22:31 -0300

    feat: new themes, using new oh-my-posh command line to intialize the prompt

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,11 @@
-Import-Module oh-my-posh
+$poshTheme = "~\.dotfiles\poshthemes\material.omp.json"
+if (!(Test-Path $poshTheme)) {
+    $poshTheme = "~/jandedobbeleer.omp.json"
+}
+
+oh-my-posh --init --shell pwsh --config $poshTheme | Invoke-Expression
 Import-Module posh-git
 Import-Module PSReadLine
-
-if ((Test-Path "~\.dotfiles\poshthemes\my-star.omp.json")) {
-    Set-PoshPrompt -Theme ~\.dotfiles\poshthemes\my-star.omp.json
-} else {
-    Set-PoshPrompt -Theme star # fallback to original oh-my-posh theme
-}
 
 Push-Location (Split-Path -Parent $profile)
 Get-ChildItem *.ps1 -Exclude "Microsoft.PowerShell_profile.ps1" | ForEach-Object -Process {Invoke-Expression ". $_"}
